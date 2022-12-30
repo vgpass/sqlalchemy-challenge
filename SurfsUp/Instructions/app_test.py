8128117f9057c2ec31b8fe65ab0441704056c8d9
Vincent_Passanisi <vince@santafeimporters.com>
2022-12-29 21:04:21 -0800

    Updating

--- SurfsUp/Instructions/app_test.py
+++ SurfsUp/Instructions/app_test.py
@@ -1,35 +1,33 @@
-from flask import Flask, jsonify
-import numpy as np
-import pandas as pd
-import datetime as dt
 import sqlalchemy
+from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, inspect
 
+# Database Setup
 # create engine to hawaii.sqlite
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
 # reflect an existing database into a new model
 Base = automap_base()
 
-
 # reflect the tables
-Base.prepare(autoload_with=engine)
+Base.prepare(engine, reflect=True)
 
 # View all of the classes that automap found
 Base.classes.keys()
+
 # Save references to each table
-Measure = Base.classes.measurement
+Measurement = Base.classes.measurement
 Station = Base.classes.station
+
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
-
-#################################################
 # Flask Setup
-#################################################
 app = Flask(__name__)
-
-
 #################################################
 # Flask Routes
 #################################################
